9618e166a0616cf01e6932be93e267784d6b1b7a
Your Namepythonvista <you@example.comnablintech@gmail.com>
2022-10-06 22:03:24 -0700

    updated

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const starwarsRoute = require('./src/routes/starwars');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/starwars', starwarsRoute);
+
+
+app.get('/', (req, res)=>{
+    res.send("we are home")
+})
+
+
+
+
+
+//connect db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true,}, ()=>{
+    console.log("connected")
+})
+
+
+
+
+
+app.listen(3000);
